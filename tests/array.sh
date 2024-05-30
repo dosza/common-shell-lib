5cda35db36686901bca98f82287429ec5c2e1298
dosza <oliveira.daniel109@gmail.com>
2024-05-30 19:45:15 -0300

    add empty array tests

--- tests/array.sh
+++ tests/array.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
 source ./get-shunit2
+testIsArrayEmpty(){
+    local array=()
+    local fake_array=''
+    local fake_array_2=0
+    
+    isArrayEmpty array 
+    assertTrue '[passing an empty array ]' $?
 
+    array=({0..10})
+    
+    isArrayEmpty array 
+    assertFalse '[passing a non-empty array]' $?
+    
+    local -n ref_array=array
+
+    assertTrue '[Deferencing an array is not supported]' $?
+    unset array 
+
+    isArrayEmpty array 
+    assertTrue '[No existent array]'  $?
+
+    isArrayEmpty fake_array_2
+    assertTrue '[ Case string variable ]' $?
+
+
+}
 testArrayToSTring(){
     local array=({0..10..2})
     local str="0 2 4 6 8 10"
@@ -26,6 +51,7 @@ testArraySlice(){
 }
 
 testArrayMap(){
+    local empty_array=()
     local packages=(gcc g++ wget)
     local str="gcc\ng++\nwget\n"
     local buttons=(button{Um,Dois,Tres})
@@ -35,6 +61,9 @@ testArrayMap(){
     assertEquals "$(arrayMap packages package 'echo $package')" "$(printf "%b" "$str")"
     assertEquals "$(arrayMap buttons button index 'echo view.findViewById\(R.id.$button\)')" "$(printf "%b" "$strButtons")"
     assertEquals "$(arrayMap buttons button index 'printf "%b" "case R.id.$button:\n\tmediaPlayer = MediaPlayer(getActivity(),R.raw.${sounds[$index]});\n\tbreak;\n"')" "$(printf "%b" "$strButtonsAct")"
+
+    arrayMap empty_array value 'echo $value'
+    assertFalse '[ passing an empty array ]' $?
 }
 
 testArrayFilter(){
@@ -43,6 +72,9 @@ testArrayFilter(){
     local impares=()
     local names=('Daniel' 'Davros' 'Elis' 'Etel')
     local matchDNames=()
+
+    arrayFilter pares index numbers '((number %2 == 0))'
+    assertFalse '[Passing an empty array]' $?
 
     arrayFilter numbers number pares '((number % 2 == 0))'
 
@@ -60,6 +92,8 @@ testArrayFilter(){
 
     arrayFilter  names name index matchDNames 'echo ${names[$index]} | grep ^D>/dev/null'
     assertEquals  "Daniel Davros" "${matchDNames[*]}"
+
+    
 }
 
 
@@ -70,6 +104,10 @@ testForEach(){
     local expected_five_table=({0..50..5})
     local five_table=({0..10})
 
+    local empty_array=()
+
+    forEach empty_array value '((value+=1))'
+    assertFalse '[passing an empty array]' $?
 
     forEach five_table number 'number=$(echo "$number * 5"| bc )'
 
